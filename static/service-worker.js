3388d754a604b702fcae3cb8ddc97252205cd973
kinesiologiahospitalmelo-blip <kinesiologia.hospital.melo@gmail.com>
2025-11-13 18:01:17 -0300

    Refactor service worker to remove caching logic
    Removed caching functionality and added console logs for install and activate events.

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,43 +1,10 @@
-const CACHE_NAME = "alertas-cache-v4";
-
-const FILES_TO_CACHE = [
-  "/static/index.html",
-  "/static/styles.css",
-  "/static/app.js",
-  "/static/manifest.json"
-];
-
-// Install
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", e => {
+    console.log("SW instalado");
+    self.skipWaiting();
 });
 
-// Activate (borra versiones viejas)
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", e => {
+    console.log("SW activado");
 });
 
-// Fetch
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
